1947ce57ede33a61a9d220d98c500277d757d622
Anatolyi1704 <Tolian2005_89@mail.ru>
2025-01-15 20:13:55 +1000

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Анатолий"
+print("Имя пользователя:", username)
+title = "Лекция по английскому"
+print("Заголовок:", title)
+content = "Bird - птица, car - автомобиль"
+print("Описание:", content)
+status = "Актуальная"
+print("Статус:", status)
+created_date = "15.01.2025"
+print("Дата создания:", created_date)
+issue_date = "30.06.2025"
+print("Дата истечения:", issue_date)

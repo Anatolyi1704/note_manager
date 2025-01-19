d89b450830e42602880b073e32a00586738fa51f
Anatolyi1704 <Tolian2005_89@mail.ru>
2025-01-19 15:57:34 +1000

    Add files via upload

--- final.py
+++ final.py
@@ -3,18 +3,18 @@ title = input ("Введите заголовок заметки: ")
 title1 = input ("Введите заголовок первой части: ")
 title2 = input ("Введите заголовок второй части: ")
 title3 = input ("Введите заголовок третьей части: ")
-title_0 = [title1, title2, title3]
+titles = [title1, title2, title3]
 content = input("Введите содержание заметки: ")
 status = input ("Введите статус заметки: ")
 created_date = input ("Введите дату создания (в формате ДД.ММ.ГГГГ): ")
 issue_date = input ("Введите дату окончания (в формате ДД.ММ.ГГГГ): ")
 note = {
-    "Имя пользователя: " : username,
+    "Имя пользователя:" : username,
     "Заголовок:" : title,
+    "Подзаголовки:": titles,
     "Содержание:" : content,
     "Статус:" : status,
     "Дата создания:" : created_date[0:5],
     "Дата окончания:" : issue_date[0:5],
-    "Подзаголовки:" : title_0
 }
 print(note)

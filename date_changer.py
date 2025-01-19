2bff249658f48be43a2518ef9703ccaf16d95e9e
Anatolyi1704 <Tolian2005_89@mail.ru>
2025-01-19 15:43:55 +1000

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,3 +1,11 @@
+username = "Анатолий"
+print("Имя пользователя:", username)
+title = "Лекция по английскому"
+print("Заголовок:", title)
+content = "Bird - птица, car - автомобиль"
+print("Описание:", content)
+status = "Актуальная"
+print("Статус:", status)
 created_date = "15.01.2025"
 print("Дата создания:", created_date[0:5])
 issue_date = "30.06.2025"
